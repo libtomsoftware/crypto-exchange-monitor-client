905075aa9ae43399c3186351127186461ce2bfa9
Tomasz Libich <tomaszlibich@Tomaszs-MacBook-Pro-2.local>
2018-07-17 12:33:16 +0100

    Fix lint

--- src/features/balances/position.js
+++ src/features/balances/position.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Position = (props) => {
     return (<div className="position">
-        <span className="position-label">{props.currency}</span> 
+        <span className="position-label">{props.currency}</span>
         <span className="position-amount">{props.amount}</span>
     </div>);
 };
